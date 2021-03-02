e138db249de3ca264bf8d213f0d762c62e0bf8e8
itsPavelXmas <pavelcristian2399@gmail.com>
2021-03-02 11:29:34 +0200

    JavaBasics Part2 Lab

--- Lab2-JavaBasics/src/ro/ase/csie/cts/g1092/w2/Account.java
+++ Lab2-JavaBasics/src/ro/ase/csie/cts/g1092/w2/Account.java
@@ -1,5 +1,11 @@
 package ro.ase.csie.cts.g1092.w2;
 
-public abstract class Account {
+import ro.ase.csie.cts.g1092.w2.exceptions.IllegalTransferException;
+import ro.ase.csie.cts.g1092.w2.exceptions.InsufficientFundsException;
 
+public abstract class Account {
+		public abstract double getBalance();
+		public abstract void deposit(double amount);
+		public abstract void transfer(Account destination,double amount) throws InsufficientFundsException, IllegalTransferException;
+		public abstract void withdraw(double amount) throws InsufficientFundsException;
 }
--- Lab2-JavaBasics/src/ro/ase/csie/cts/g1092/w2/test/TestApp.java
+++ Lab2-JavaBasics/src/ro/ase/csie/cts/g1092/w2/test/TestApp.java
@@ -1,10 +1,12 @@
 package ro.ase.csie.cts.g1092.w2.test;
 
+import ro.ase.csie.cts.g1092.w2.interest.Profitable;
+
 public class TestApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//Profitable.MAX_INTEREST_PERCENT=10;	
 	}
 
 }
