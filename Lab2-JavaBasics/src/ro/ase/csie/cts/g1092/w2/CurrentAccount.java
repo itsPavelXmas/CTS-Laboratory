f242ea94705fe363257af0f9d286b2005e4a597a
itsPavelXmas <pavelcristian2399@gmail.com>
2021-03-02 11:31:11 +0200

    All Lab2- Java Basics

--- Lab2-JavaBasics/src/ro/ase/csie/cts/g1092/w2/CurrentAccount.java
+++ Lab2-JavaBasics/src/ro/ase/csie/cts/g1092/w2/CurrentAccount.java
@@ -1,22 +1,38 @@
 package ro.ase.csie.cts.g1092.w2;
 
-public class CurrentAccount extends BankAccount {
+import ro.ase.csie.cts.g1092.w2.exceptions.IllegalTransferException;
+import ro.ase.csie.cts.g1092.w2.exceptions.InsufficientFundsException;
 
+public class CurrentAccount extends BankAccount {
+	
+	public static final double MAX_CREDIT=5000;
+
+	public CurrentAccount(String IBAN) {
+		super(CurrentAccount.MAX_CREDIT,IBAN);
+	}
+	
 	@Override
 	public void deposit(double amount) {
-		// TODO Auto-generated method stub
+		this.balance+=amount;
 
 	}
 
 	@Override
-	public void transfer(Account destination, double amount) {
-		// TODO Auto-generated method stub
+	public void transfer(Account destination, double amount)
+			throws InsufficientFundsException, IllegalTransferException {
+		if(this == destination)
+			throw new IllegalTransferException();
+		this.withdraw(amount);
+		destination.deposit(amount);
 
 	}
 
 	@Override
-	public void withdraw(double amount) {
-		// TODO Auto-generated method stub
+	public void withdraw(double amount) throws InsufficientFundsException {
+		if(amount >this.balance)
+			throw new InsufficientFundsException("You don't have the amount ");
+		else
+			this.balance -=amount;
 
 	}
 
